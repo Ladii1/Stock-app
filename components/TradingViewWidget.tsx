d515a71bd5aaca863f870b4a38225f71c22a0fcb
Ladii1 <faroukawe201@gmail.com>
2025-10-03 03:42:45 +0100

    Add TradingView widgets and configuration constants

---
+++ components/TradingViewWidget.tsx
@@ -0,0 +1,34 @@
+'use client'
+import useTradingviewWidget from '@/hooks/useTradingviewWidget';
+import { cn } from '@/lib/utils';
+import React, {  useRef, memo } from 'react';
+
+interface TradingViewWidgetProps {
+  title?: string;
+  scriptUrl?: string;
+  config?: string; 
+  height?: number;
+  className?: string;
+}
+
+function TradingViewWidget({
+    title, 
+    scriptUrl, 
+    config, 
+    height = 600, 
+    className
+    }: TradingViewWidgetProps) {
+  const containerRef = useTradingviewWidget( scriptUrl, config, height);
+
+  return (
+    <div className='w-full'>
+      {title && <h3 className='font-semibold text-2xl text-gray-100 mb-5'>{title}</h3>}
+      <div className={cn("tradingview-widget-container",className)} ref={containerRef} >
+        <div className="tradingview-widget-container__widget" style={{ height, width: "100%" }} />
+      </div>
+    </div>
+    
+  );
+}
+
+export default memo(TradingViewWidget);
